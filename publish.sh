15f7a2b5ecc86967f7acf3e35a4d123d8a2274da
kenji yoshida <6b656e6a69@gmail.com>
2021-05-28 12:28:52 +0900

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -4,10 +4,10 @@ sbt \
     ++2.11.12 \
     clean \
     publishSigned \
-    ++2.12.11 \
+    ++2.12.14 \
     clean \
     publishSigned \
-    ++2.13.2 \
+    ++2.13.6 \
     clean \
     publishSigned \
     "project plugin" \
